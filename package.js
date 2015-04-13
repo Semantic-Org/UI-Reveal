a564f6993b436f4323538f7b2f949877d264d1ab
Pusher Robot <jack@semantic-ui.com>
2015-04-13 15:23:55 -0400

    Updated component to version 1.11.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-reveal',
   summary : 'Semantic UI - Reveal: Single component release',
-  version : '1.11.6',
+  version : '1.11.7',
   git     : 'git://github.com/Semantic-Org/UI-Reveal.git',
 });
 
