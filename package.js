7248fcb120ef31042dd13a8592cf4f116bb288d1
Jack Lukic <jack@semantic-ui.com>
2017-02-21 14:24:25 -0500

    Updated component to version 2.2.9

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-reveal',
   summary : 'Semantic UI - Reveal: Single component release',
-  version : '2.2.8',
+  version : '2.2.9',
   git     : 'git://github.com/Semantic-Org/UI-Reveal.git',
 });
 
